63698862d28b9172cfb3c98c9f4656c5c67e5d15
sowmik-sec <48240954+sowmik-sec@users.noreply.github.com>
2022-05-31 22:45:26 +0600

    I've solved this many times Alhamdulillah

---
+++ Recursion/Factorial of a Number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+	static int factorial(int n) {
+        if(n==0) return 1;
+        int partialAns = factorial(n-1);
+        return n*partialAns;
+    }
+	public static void main(String args[]) {
+		
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n<0) System.out.println("Error");
+        else System.out.println(factorial(n));
+		
+	}
+    
+}
